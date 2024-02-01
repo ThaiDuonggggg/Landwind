a9be5306dedc55cc117d73315c9f35c518575133
ThaiDuonggggg <hongdyong990@gmail.com>
2024-02-01 15:47:59 +0700

    Add JS

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,3 +6,25 @@ function showNavMobile() {
 }
 
 barsMobile.addEventListener('click', showNavMobile);
+
+
+/* show question-list*/
+
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document);
+
+const askLists = $$('.asked-question__list');
+const askDess = $$('.asked-question__description');
+
+askLists.forEach((aks, index) => {
+    const askDes = askDess[index];
+
+    aks.onclick = function () {
+        // $('.asked-question__list.active').classList.remove('active');
+        // $('.asked-question__description.active').classList.remove('active');
+
+
+        this.classList.toggle('active')
+        askDes.classList.toggle('active')
+    }
+})
